c9ebc31cbf2bc95efb4fad0971a9614f4496764b
Anton-Lithium <antonlitvincev525@gmail.com>
2026-05-20 23:26:46 +0300

    KS-3-3

--- KS-3-3.kt
+++ KS-3-3.kt
@@ -1,6 +1,4 @@
 fun main() {
     val num = 6
-    for (i in 1..9){
-        println("$num x $i = ${num * i}")
-    }
+    println("$num x 1 = ${num * 1} $num x 2 = ${num * 2} $num x 3 = ${num * 3} $num x 4 = ${num * 4} $num x 5 = ${num * 5} $num x 6 = ${num * 6} $num x 7 = ${num * 7} $num x 8 = ${num * 8} $num x 9 = ${num * 9}")
 }
