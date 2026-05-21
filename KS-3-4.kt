57a31c95cd58546160a437fbd1b2d7c431b3681b
Anton-Lithium <antonlitvincev525@gmail.com>
2026-05-21 08:10:02 +0300

    KS-3-4

--- KS-3-4.kt
+++ KS-3-4.kt
@@ -2,9 +2,9 @@ fun main() {
     var startTurn = "E2"
     var finishTurn = "E4"
     var numTurn = 1
-    println("[$startTurn-$finishTurn;$numTurn]")
+    println("$startTurn-$finishTurn;$numTurn")
     startTurn = "D2"
     finishTurn = "D3"
     numTurn++
-    println("[$startTurn-$finishTurn;$numTurn]")
+    println("$startTurn-$finishTurn;$numTurn")
 }
