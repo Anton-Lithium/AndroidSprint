e4b636c732f724fa40f66a8884290b0b40bae748
Anton-Lithium <antonlitvincev525@gmail.com>
2026-05-23 21:37:02 +0300

    KS-4-3

--- KS-4-3.kt
+++ KS-4-3.kt
@@ -4,11 +4,11 @@ const val FAVORABLE_AIR_HUMIDITY = 20
 const val WINTER_SEASON = "зима"
 
 fun main() {
-    val isSunWeather = true
+    val isSunnyWeather = true
     val isAwningOpen = true
     val airHumidity = 20
     val season = "зима"
-    val result = isSunWeather == IS_WEATHER_SUNNY && isAwningOpen == IS_AWNING_OPEN &&
+    val isFavorableConditions = isSunnyWeather == IS_WEATHER_SUNNY && isAwningOpen == IS_AWNING_OPEN &&
             airHumidity == FAVORABLE_AIR_HUMIDITY && season != WINTER_SEASON
-    println("Благоприятные ли условия сейчас для роста бобовых? $result")
+    println("Благоприятные ли условия сейчас для роста бобовых? $isFavorableConditions")
 }
