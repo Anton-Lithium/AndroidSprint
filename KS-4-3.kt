07db3a0ae3f1ccf63e7395fd115b0c8c42a5421c
Anton-Lithium <antonlitvincev525@gmail.com>
2026-05-23 21:20:45 +0300

    KS-4-3

--- KS-4-3.kt
+++ KS-4-3.kt
@@ -2,11 +2,13 @@ const val IS_WEATHER_SUNNY = true
 const val IS_AWNING_OPEN = true
 const val FAVORABLE_AIR_HUMIDITY = 20
 const val WINTER_SEASON = "зима"
+
 fun main() {
     val sunWeatherNow = true
     val isAwningOpenNow = true
     val airHumidityNow = 20
     val season = "зима"
-    val result = sunWeatherNow && isAwningOpenNow && airHumidityNow == FAVORABLE_AIR_HUMIDITY && season != WINTER_SEASON
+    val result = sunWeatherNow == IS_WEATHER_SUNNY && isAwningOpenNow == IS_AWNING_OPEN &&
+            airHumidityNow == FAVORABLE_AIR_HUMIDITY && season != WINTER_SEASON
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
 }
