1c1bf22f7a8317ba4191e9caabd088379c4e1095
Anton-Lithium <antonlitvincev525@gmail.com>
2026-05-24 15:42:30 +0300

    KS-4-4

--- KS-4-4.kt
+++ KS-4-4.kt
@@ -1,8 +1,8 @@
 fun main() {
     val trainingDayNumber = 5
     val isEvenDay = trainingDayNumber % 2 == 0
-    println("Упражнения для рук:    $isEvenDay\n" +
-            "Упражнения для ног:    ${!isEvenDay}\n" +
-            "Упражнения для спины:  ${!isEvenDay}\n" +
-            "Упражнения для пресса: $isEvenDay")
+    println("Упражнения для рук:    ${!isEvenDay}\n" +
+            "Упражнения для ног:    ${isEvenDay}\n" +
+            "Упражнения для спины:  ${isEvenDay}\n" +
+            "Упражнения для пресса: ${!isEvenDay}")
 }
