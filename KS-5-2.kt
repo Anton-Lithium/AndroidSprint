1941892ec70f1b24b95b5133a7aa986a4339203e
Anton-Lithium <antonlitvincev525@gmail.com>
2026-05-26 23:02:27 +0300

    KS-5-2

---
+++ KS-5-2.kt
@@ -0,0 +1,9 @@
+import java.time.LocalDate
+const val AGE_OF_MAJORITY = 18
+fun main() {
+    val currentYear = LocalDate.now().year
+    println("В каком году Вы родились?")
+    val yearBirth = readln().toInt()
+    if (currentYear - yearBirth > AGE_OF_MAJORITY) println ("Показать экран со скрытым контентом")
+    else println("Доступ запрещен.")
+}
