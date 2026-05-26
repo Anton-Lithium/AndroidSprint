a292c85418de76f0bb7aa6ce5897cbc56a58507a
Anton-Lithium <antonlitvincev525@gmail.com>
2026-05-26 22:15:53 +0300

    KS-4-5

---
+++ KS-5-1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Сколько будет 2 + 3")
+    val resultNumber = readln().toInt()
+    if (resultNumber == 5) {
+        println("Добро пожаловать!")
+    }
+    else {
+        println("Доступ запрещен.")
+    }
+}
