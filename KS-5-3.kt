e563afacfae55f940bead17e1fc14919ba9a1d67
Anton-Lithium <antonlitvincev525@gmail.com>
2026-05-28 20:53:23 +0300

    KS-5-3

--- KS-5-3.kt
+++ KS-5-3.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val currentNumber1 =12 //(0..42).random()
-    val currentNumber2 =21 //(0..42).random()
+    val currentNumber1 =(0..42).random()
+    val currentNumber2 =(0..42).random()
     println("Добро пожаловать в лотерею!")
     println("Вам нужно угадать два числа от 0 до 42.")
     println("Если угадаете хотя бы одно — получите утешительный приз!")
@@ -8,15 +8,13 @@ fun main() {
     val userNumber1 = readln().toInt()
     print("Введите второе число (от 0 до 42):")
     val userNumber2 = readln().toInt()
-    //val firstCoincidence = (currentNumber1 == userNumber1) || (currentNumber2 == userNumber1)
-    //val secondCoincidence = (currentNumber1 == userNumber2) || (currentNumber2 == userNumber2)
     if ((currentNumber1 == userNumber1 && currentNumber2 == userNumber2) ||
         (currentNumber2 == userNumber1 && currentNumber1 == userNumber2)) {
-        println("Поздравляем! Вы выиграли главный приз!")
+        println("Поздравляем! Вы выиграли главный приз!\nЗагаданные числа $currentNumber1 и $currentNumber2")
     } else if (currentNumber1 == userNumber1 || currentNumber2 == userNumber2 ||
         currentNumber1 == userNumber2 || currentNumber2 == userNumber1) {
-        println("Вы выиграли утешительный приз!")
+        println("Вы выиграли утешительный приз!\nЗагаданные числа $currentNumber1 и $currentNumber2")
     } else {
-        println("Неудача!")
+        println("Неудача!\nЗагаданные числа $currentNumber1 и $currentNumber2")
     }
 }
